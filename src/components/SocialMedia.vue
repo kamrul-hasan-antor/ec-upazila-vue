b12f2c4a4093825c30461986da0e9b34a272bf4d
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2026-04-16 12:26:15 +0600

    social media section done

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="bg-[#151C29] py-4">
     <div class="container">
-      <div class="flex justify-between items-center">
+      <div class="flex flex-col sm:flex-row justify-between items-center gap-4">
         <p class="text-sm text-white">
           বাংলাদেশ নির্বাচন কমিশন – উপজেলা পোর্টাল
         </p>
 
-        <div class="flex">
+        <div class="flex gap-4">
           <a
             href="/"
-            class="flex items-center gap-2 bg-blue-500 px-2 py-1 rounded text-white"
+            class="flex items-center gap-2 bg-[#1877F2] px-3 py-1 rounded text-white"
           >
             <svg
               width="9"
@@ -27,6 +27,42 @@
               />
             </svg>
             <span class="text-sm">ফেসবুক</span>
+          </a>
+          <a
+            href="/"
+            class="flex items-center gap-2 bg-[#FF0000] px-3 py-1 rounded text-white"
+          >
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0_3104_4660)">
+                <path
+                  d="M1.66667 11.3333C1.20095 9.13551 1.20095 6.86449 1.66667 4.66667C1.72786 4.44347 1.8461 4.24005 2.00974 4.0764C2.17339 3.91276 2.37681 3.79453 2.6 3.73333C6.17564 3.14097 9.82437 3.14097 13.4 3.73333C13.6232 3.79453 13.8266 3.91276 13.9903 4.0764C14.1539 4.24005 14.2721 4.44347 14.3333 4.66667C14.7991 6.86449 14.7991 9.13551 14.3333 11.3333C14.2721 11.5565 14.1539 11.7599 13.9903 11.9236C13.8266 12.0872 13.6232 12.2055 13.4 12.2667C9.82438 12.8591 6.17563 12.8591 2.6 12.2667C2.37681 12.2055 2.17339 12.0872 2.00974 11.9236C1.8461 11.7599 1.72786 11.5565 1.66667 11.3333Z"
+                  stroke="white"
+                  stroke-width="1.33333"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M6.66669 10L10 8L6.66669 6V10Z"
+                  stroke="white"
+                  stroke-width="1.33333"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_3104_4660">
+                  <rect width="16" height="16" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+
+            <span class="text-sm pt-1">ইউটিউব</span>
           </a>
         </div>
       </div>
