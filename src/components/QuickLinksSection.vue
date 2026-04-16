f2faf25c66d0c255ec08ae953c085f9e85042b20
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2026-04-16 18:45:38 +0600

    select option desing done

--- src/components/QuickLinksSection.vue
+++ src/components/QuickLinksSection.vue
@@ -2,6 +2,7 @@
 import { FileText, Info, Phone, Vote } from "@lucide/vue";
 import QuickLinksCards from "./QuickLinksCards.vue";
 import { h } from "vue";
+import SearchBox from "./SearchBox.vue";
 
 const quickLinks = [
   {
@@ -31,41 +32,41 @@ const quickLinks = [
 <template>
   <div class="bg-[#F9FAFB] pb-16">
     <div class="container px-4">
-      <!-- Overlapping Card -->
-      <div
-        class="relative z-10 -mt-12 md:-mt-16 bg-white rounded-xl shadow-[0px_8px_40px_0px_#00000025] border border-gray-100 px-6 py-5 md:px-10 md:py-7"
-      >
-        <h2
-          class="text-base md:text-lg font-semibold text-gray-700 mb-4 border-l-4 border-[#009951] pl-3"
-        >
-          উপজেলা তথ্য অনুসন্ধান
-        </h2>
-
-        <!-- Info note -->
-
+      <div class="max-w-5xl mx-auto">
         <div
-          class="mt-2 flex items-start sm:items-center gap-2 bg-[#F0FDF4] border-l-4 border border-[#00814E] rounded-md px-4 py-3 text-sm text-[#374151]"
+          class="relative z-10 -mt-12 md:-mt-16 bg-white rounded-xl shadow-[0px_8px_40px_0px_#00000025] border border-gray-100 px-6 py-5 md:px-10 md:py-7"
         >
-          <Info class="size-5 shrink-0" />
-          <span class="pt-0.5"
-            >আপনার অঞ্চল, জেলা, উপজেলা ও থানা নির্বাচন করুন। স্থানীয় অফিসের
-            বিস্তারিত তথ্য, নোটিশ, জনসেবা এবং এলাকাভিত্তিক গুরুত্বপূর্ণ তথ্য
-            দেখতে পাবেন।</span
+          <h2
+            class="text-base md:text-lg font-semibold text-gray-700 mb-4 border-l-4 border-[#009951] pl-3"
           >
-        </div>
-      </div>
+            উপজেলা তথ্য অনুসন্ধান
+          </h2>
 
-      <!-- Quick Links -->
-      <div class="grid md:grid-cols-3 gap-6 mt-10">
-        <QuickLinksCards
-          v-for="link in quickLinks"
-          :key="link.title"
-          :title="link.title"
-          :description="link.description"
-          :color="link.color"
-          :url="link.url"
-          :icon="link.icon"
-        />
+          <SearchBox />
+
+          <div
+            class="mt-2 flex items-start sm:items-center gap-2 bg-[#F0FDF4] border-l-4 border border-[#00814E] rounded-md px-4 py-3 text-sm text-[#374151]"
+          >
+            <Info class="size-5 shrink-0" />
+            <span class="pt-0.5"
+              >আপনার অঞ্চল, জেলা, উপজেলা ও থানা নির্বাচন করুন। স্থানীয় অফিসের
+              বিস্তারিত তথ্য, নোটিশ, জনসেবা এবং এলাকাভিত্তিক গুরুত্বপূর্ণ তথ্য
+              দেখতে পাবেন।</span
+            >
+          </div>
+        </div>
+
+        <div class="grid md:grid-cols-3 gap-6 mt-10">
+          <QuickLinksCards
+            v-for="link in quickLinks"
+            :key="link.title"
+            :title="link.title"
+            :description="link.description"
+            :color="link.color"
+            :url="link.url"
+            :icon="link.icon"
+          />
+        </div>
       </div>
     </div>
   </div>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,101 @@
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
+<!-- <style>
+.multiselect__option--highlight {
+  background: #6b7280 !important;
+  color: white !important;
+}
+
+.multiselect__option--highlight::after {
+  background: #6b7280 !important;
+  content: "" !important;
+}
+
+.multiselect__option--selected.multiselect__option--highlight::after {
+  content: "" !important;
+}
+
+.multiselect--active .multiselect__placeholder {
+  color: #16a34a !important;
+}
+
+.multiselect--active .multiselect__tags {
+  border-color: #3b82f6 !important;
+  box-shadow: 0 0 0 2px rgba(59, 130, 246, 0.3) !important;
+}
+
+/* Hide the caret/arrow since it's always open */
+.multiselect__select {
+  display: none;
+}
+
+/* Always show dropdown */
+.multiselect__content-wrapper {
+  display: block !important;
+}
+</style> -->
+
+<style>
+.multiselect__input::placeholder {
+  color: #adadad;
+  font-size: 14px;
+}
+</style>
+
+<script>
+import { ChevronDown } from "@lucide/vue";
+import Multiselect from "vue-multiselect";
+
+export default {
+  components: { Multiselect, ChevronDown },
+  data() {
+    return {
+      isOpen: false,
+      value: null,
+      options: [
+        { name: "Vue.js", language: "JavaScript" },
+        { name: "Rails", language: "Ruby" },
+        { name: "Laravel", language: "PHP" },
+        { name: "Phoenix", language: "Elixir" },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="grid grid-cols-4">
+    <multiselect
+      ref="multiselect"
+      v-model="value"
+      :options="options"
+      placeholder="অঞ্চল নির্বাচন করুন"
+      track-by="name"
+      label="language"
+      selectedLabel=""
+      selectLabel=""
+      :max-height="150"
+      @open="isOpen = true"
+      @close="isOpen = false"
+    >
+      <template #noResult>
+        <p class="text-center">Not Found.</p>
+      </template>
+
+      <template #placeholder>
+        <span class="pl-1.25">অঞ্চল নির্বাচন করুন</span>
+      </template>
+
+      <template #caret>
+        <div
+          class="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer transition-transform duration-200"
+          :class="{ 'rotate-180': isOpen }"
+        >
+          <ChevronDown size="18" color="#000" />
+        </div>
+      </template>
+    </multiselect>
+  </div>
+
+  <div></div>
+</template>
